af35877b969291f70f0eafb5e7ee0fbdbc26e899
Hitesh98 <38819734+Hitesh98@users.noreply.github.com>
2018-12-31 12:34:31 +0530

    Update LCS.java

--- LCS.java
+++ LCS.java
@@ -1,15 +1,20 @@
 public class LCS {
 
+    // Method to find the LCS length and common characters
     private char[] findLCS(String s1, String s2) {
-
+        
+        // Create character array out of the Strings.
+        // This can be avoided by using String.charAt(index) directly.
         char[] X = s1.toCharArray();
         char[] Y = s2.toCharArray();
         int m = X.length;
         int n = Y.length;
         int[][] lcsTable = new int[m+1][n+1];
-
+        
+        // First set of for loops to create the lcs table
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
+                // Initialise first row and column as zero (mayn not be needed as java initialises intergers to zero by default
                 if (i == 0 || j == 0) {
                     lcsTable[i][j] = 0;
                 } else if (X[i-1] == Y[j-1]) {
@@ -19,6 +24,8 @@ public class LCS {
                 }
             }
         }
+        
+        // One while loop to find all common alphabets using the lcs table
         char[] commonArray = new char[lcsTable[m][n]];
         int index = lcsTable[m][n]-1;
         int i = m, j = n;
@@ -35,11 +42,13 @@ public class LCS {
         }
         return commonArray;
     }
-
+    
+    // Utility method to return the maximum of two integers
     private static int max(int a, int b) {
         return (a > b)? a : b;
     }
 
+    // Driver method
     public static void main(String[] args) {
         LCS lcsobj = new LCS();
         String s1 = "AGGTAB";
